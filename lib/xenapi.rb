15093017e897f091d3cdcec5f93044f52435218d
PotHix <pothix@pothix.com>
2011-11-18 17:55:59 -0200

    Fixing the require order

--- lib/xenapi.rb
+++ lib/xenapi.rb
@@ -1,10 +1,12 @@
 # -*- encoding: utf-8 -*-
 require "yaml"
-require "xenapi/session"
-require "xenapi/error"
 require "xenapi/virtual_machine"
 require "xenapi/storage"
 require "xenapi/vdi"
 require "xenapi/vdb"
 require "xenapi/task"
+require "xenapi/network"
+
+require "xenapi/session"
+require "xenapi/error"
 require "xenapi/hypervisor_connection"
--- lib/xenapi/version.rb
+++ lib/xenapi/version.rb
@@ -2,7 +2,7 @@ module XenAPI
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 16
+    PATCH = 17
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
