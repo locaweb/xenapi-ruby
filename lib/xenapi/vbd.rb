0b2de63c0c8ee90863c9cea9f923705350f48c00
PotHix <pothix@pothix.com>
2011-11-24 10:48:46 -0200

    Renaming the module and removing the on_hypervisor method

--- lib/xenapi/vbd.rb
+++ lib/xenapi/vbd.rb
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 module XenAPI
-  module Vdb
+  module Vbd
     def create_VBD_for(vm_ref, disk_uuid, disk_number)
       self.VBD.create({
         :VM => vm_ref,
-        :VDI => self.on_hypervisor.VDI.get_by_uuid(disk_uuid),
+        :VDI => self.VDI.get_by_uuid(disk_uuid),
         :userdevice => disk_number.to_s,
         :bootable => false,
         :mode => "RW",
--- lib/xenapi/version.rb
+++ lib/xenapi/version.rb
@@ -2,7 +2,7 @@ module XenAPI
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 24
+    PATCH = 25
     STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
   end
 end
